2ca7cc2be1fe37302cd8f7e914cd10e3702a3b29
sgkikas <stylianos.gkikas@ext.europarl.europa.eu>
2026-06-08 10:53:36 +0200

    making bio html

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { BlogComponent } from './components/blog/blog.component';
+import { BioComponent } from './components/bio/bio.component';
+
 
 export const routes: Routes = [
   {
     path: 'blog',
     component: BlogComponent
-  }
+  },
+  { path: 'bio', 
+    component: BioComponent }
+
+
 ];
