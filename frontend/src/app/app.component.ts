31382bc6667ea88909d26c103d4f85e5b2fadf34
sgkikas <stylianos.gkikas@ext.europarl.europa.eu>
2026-06-08 16:44:35 +0200

    adding project component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,25 +3,22 @@ import { CommonModule } from '@angular/common';
 import { BlogComponent } from './components/blog/blog.component';
 import { RequestsComponent } from './components/requests/request.component';
 import { BioComponent } from './components/bio/bio.component';
+import { ProjectComponent } from './components/project/project.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, BlogComponent, RequestsComponent, BioComponent],
+  imports: [CommonModule, BlogComponent, RequestsComponent, BioComponent, ProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  activePage: 'blog' | 'requests' | 'bio' | null = null;
+  activePage: 'blog' | 'requests' | 'bio' | 'project' | null = null;
 
   goToBlog(): void {
     this.activePage = 'blog';
   }
-
-  //  goToHome(): void {
-  //   this.activePage = 'home';
-  // }
 
   goToRequests(): void {
     this.activePage = 'requests';
@@ -29,5 +26,9 @@ export class AppComponent {
 
   goToBio(): void {
     this.activePage = 'bio';
+  }
+
+    goToProject(): void {
+    this.activePage = 'project';
   }
 }
